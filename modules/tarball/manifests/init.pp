8ac4a2b2dca3c13f7605de0bbd12ec9d8059658c
Glen Lockhart <glen@openet-dublin>
2016-10-18 14:51:55 +0100

    Updates to support remote tarballs

--- modules/tarball/manifests/init.pp
+++ modules/tarball/manifests/init.pp
@@ -1,21 +1,52 @@
-define tarball($module_name, $install_dir, $user_name, $group_name) {
+define remote_file($remote_location=undef, $mode='0644'){
+  exec{"retrieve_${title}":
+    command => "/usr/bin/wget -q ${remote_location} -O ${title}",
+    creates => $title,
+  }
 
-    file { "download file $title":
-        path    => "/tmp/$title",
-        source  => "puppet:///modules/$module_name/$title",
-		owner  => 'vagrant',
-        group  => 'vagrant',
-        mode   => '0644',
-		ensure => present
-    }
+  file{$title:
+    mode    => $mode,
+    require => Exec["retrieve_${title}"],
+  }
+}
+define tarball(
+    $module_name, 
+	$install_dir, 
+	$user_name, 
+	$group_name,
+	$tarball_file_url = '') {
+
+	if($tarball_file_url == '') {
+        file { "download file $title":
+            path    => "/tmp/$title",
+            source  => "puppet:///modules/$module_name/$title",
+		    owner   => 'vagrant',
+            group   => 'vagrant',
+            mode    => '0644',
+		    ensure  => present
+        }
+	} else {
+		remote_file{"/tmp/$title":
+            remote_location => "$tarball_file_url",
+            mode            => '0755',
+        }
+	}
 	
 	exec { "change_owner $title":
 		   command => "/bin/chown -R $user_name:$group_name $install_dir",
 		   require => Exec["untar $title"]
    }
-    # untar the tarball at the desired location
-    exec { "untar $title":
-        command     => "/bin/mkdir -p $install_dir; /bin/tar xzvf /tmp/$title -C $install_dir/",
-		require => File["download file $title"],
-    } 
+    if($tarball_file_url == '') {
+		# untar the tarball at the desired location
+		exec { "untar $title":
+			command => "/bin/mkdir -p $install_dir; /bin/tar xzvf /tmp/$title -C $install_dir/",
+			require => File["download file $title"],
+		} 
+	} else {
+		# untar the tarball at the desired location
+		exec { "untar $title":
+			command => "/bin/mkdir -p $install_dir; /bin/tar xzvf /tmp/$title -C $install_dir/",
+			require => remote_file["/tmp/$title"],
+		}
+	}
 }
