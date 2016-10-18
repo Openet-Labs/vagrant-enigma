8f2f511ed051ea4e464d565af5c679d6634c5f52
Glen Lockhart <glen@openet.com>
2016-10-18 14:55:29 +0100

    Updated to not re-install if already installed

--- modules/enigma/manifests/install.pp
+++ modules/enigma/manifests/install.pp
@@ -3,22 +3,30 @@ class enigma::install inherits enigma {
 	
 	exec { 'install_framework':
 	    command => '/bin/bash /home/vagrant/enigma_installers/enigma-framework-installer/bin/install-enigma-framework.sh',
+		user    => 'vagrant',
+		creates => "/home/vagrant/enigma",
     }
 	exec { 'install_file_reader':
 	    command => "bash -c 'source /home/vagrant/enigma/env-enigma && /home/vagrant/enigma_installers/enigma-FileReader-installer/bin/install-enigma-FileReader.sh'",
 		path    => '/usr/local/bin:/usr/bin:/bin:/usr/local/sbin:/usr/sbin:/sbin',
 		require => Exec['install_framework'],
+		user    => 'vagrant',
+		creates => "/home/vagrant/enigma",
     }
 	exec { 'install_jobs_executor':
 	    command => "bash -c 'source /home/vagrant/enigma/env-enigma && /home/vagrant/enigma_installers/enigma-JobsExecutor-installer/bin/install-enigma-JobsExecutor.sh'",
 		path    => '/usr/local/bin:/usr/bin:/bin:/usr/local/sbin:/usr/sbin:/sbin',
 		require => Exec['install_file_reader'],
+		user    => 'vagrant',
+		creates => "/home/vagrant/enigma",
     }
 	/*
 	exec { 'install_usecases':
 	    command => "bash -c 'source /home/vagrant/enigma/env-enigma && /home/vagrant/enigma_installers/enigma-usecases-installer/bin/install-enigma-usecases.sh'",
 		path    => '/usr/local/bin:/usr/bin:/bin:/usr/local/sbin:/usr/sbin:/sbin', 
 		require => Exec['install_jobs_executor'],
+		user    => 'vagrant',
+		creates  => "/home/vagrant/enigma",
     }
 	*/
 }
